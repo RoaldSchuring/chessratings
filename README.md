6342deb27c0a247db7a5ae86ea2f52c98dd9ed34
roald.schuring@hotmail.com <roald.schuring@hotmail.com>
2021-05-20 17:00:43 +0200

    modifications to setup.py file

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-If you would like to help develop this packag, along with the tools you need to develop and run tests, run the following in your virtualenv:
+
+
+## Chessratings
+
+## Installation
+
+## Usage
+
+
+
+
+
+
+If you would like to help develop this package, along with the tools you need to develop and run tests, run the following in your virtualenv:
 
 ```bash
 $ pip install -e .[dev]
--- setup.py
+++ setup.py
@@ -9,18 +9,26 @@ setup(
     description='Various chess rating systems',
     py_modules=['uscf_elo'],
     package_dir={'': 'src'},
-    # classifiers=https://pypi.org/classifiers/,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Games/Entertainment :: Board Games",
+        "License :: OSI Approved :: MIT License",
+    ],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    # install_requires = [
-    #     "numpy"
-    # ],
+    install_requires=[
+        "numpy",
+        "datetime"
+    ],
     extras_require={
         "dev": [
             "pytest>=3.7",
         ],
     },
-    url="https://github.com/RoaldSchuring/chess_ratings",
+    url="https://github.com/RoaldSchuring/chessratings",
     author="Roald Schuring",
     author_email="roald.schuring@hotmail.com"
 )
